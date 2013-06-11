8bff9e7e1fba78812e068c0713382afc71a87666
ijmorgado <i.jimenez.morgado@gmail.com>
2013-06-11 13:23:16 -0500

    Note model created, also NoteController with the CRUD set actions working properly.

---
+++ MundoJoven/Models/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MundoJoven.Models
+{
+    public class Note
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Contents { get; set; }
+        public DateTime date_creation { get; set; }
+        public DateTime date_publish { get; set; }
+        public int active { get; set; }
+
+    }
+    public class NoteDBContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+    }
+}
